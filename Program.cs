551f6326f4fd712528c50a74fda3e63b700815cd
Vladislav Taranenko <vtaranenko4@gmail.com>
2020-03-05 17:49:16 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Лабораторная_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Lab_1, Task 18\n");
+            Console.WriteLine("Insert the number: ");
+
+            double n = Convert.ToDouble(Console.ReadLine());
+            double result = Math.Pow((n), 3);
+
+            
+            Console.WriteLine("\nResult: " + result);
+        }
+    }
+}
